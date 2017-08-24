13a90066704c1710d0579244c2677b8653be4f06
pyan83 <puyanwei@gmail.com>
2017-08-24 21:08:47 +0100

    refactoring attempts to add hit points

--- app.rb
+++ app.rb
@@ -8,20 +8,21 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
+    $player_1 = Player.new(params[:player_1])
+    $player_2 = Player.new(params[:player_2])
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -15,4 +15,17 @@ describe Player do
       expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(mittens).to receive(:receive_damage)
+      dave.attack(mittens)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
+    end
+  end
 end
